528d45315d942c97f40cddc56ea12e717c7d3333
Giovanna Ferreira <gio-vannah@hotmail.com>
2025-03-20 21:00:05 -0300

    add spring boot

---
+++ aluga-ai/src/test/java/com/alugaai/aluga_ai/AlugaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alugaai.aluga_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlugaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
