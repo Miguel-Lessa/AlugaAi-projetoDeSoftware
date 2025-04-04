260dbee15d376e439549ddf1981f0e2435827182
Miguel-Lessa <meu1emeio1soh1meu@gmail.com>
2025-04-04 04:31:26 -0300

    minor alterations e home (incompleta)
    termino hoje a tarde ou amanhã de madrugada (eu preciso dormir são 4:30)

--- aluga-ai/web/src/main/java/com/alugaai/web/WebApplication.java
+++ aluga-ai/web/src/main/java/com/alugaai/web/WebApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.alugaai.web.model, com.alugai.model")
+@EntityScan(basePackages = "com.alugai.model, com.alugaai.web.model")
 public class WebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
